0903253298391b17b3fbe7b5a2cf8079d1510074
hkernbach <h.kernbach@triagens.de>
2013-07-26 13:22:51 +0200

    updated cms version

--- cms.js
+++ cms.js
@@ -1,27 +1,25 @@
 (function() {
   "use strict";
   var FoxxApplication = require("org/arangodb/foxx").Application,
-    app = new FoxxApplication();
-    
-  app.registerRepository(
+    app = new FoxxApplication(applicationContext);
+
+  var structs = app.createRepository(
     "structs",
     {
       repository: "repositories/structures"
     }
   );
-  
+
   app.get('/list', function (req, res) {
-    res.json(repositories.structs.getMonitored());
+    res.json(structs.getMonitored());
   })
-  .nickname("list")
   .summary("List all.")
   .notes("Gets a List of all monitored collections.");
-  
+
   app.get('/structs/:name', function (req, res) {
     var name = req.params("name");
-    res.json(repositories.structs.getStructs(name));
+    res.json(structs.getStructs(name));
   })
-  .nickname("structs")
   .pathParam("name", {
     description: "The name of a monitored collection",
     dataType: "string",
@@ -30,44 +28,40 @@
   })
   .summary("Get Structs of collection")
   .notes("Gets the structural information of the given collection.");
-  
+
   app.get('/content/:name', function (req, res) {
     var name = req.params("name");
-    res.json(repositories.structs.getContent(name));
+    res.json(structs.getContent(name));
   })
-  .nickname("content")
   .summary("Get the content")
   .notes("Collects the complete content of one collection.");
-  
+
   app.put("/content/:name/:key", function (req, res) {
     var name = req.params("name"),
       key = req.params("key"),
       data = JSON.parse(req.requestBody);
-    res.json(repositories.structs.updateDoc(name, key, data));
+    res.json(structs.updateDoc(name, key, data));
   })
-  .nickname("content")
   .summary("Edit one entry")
   .notes("Edit one item of a collection.");
-  
+
   app.del("/content/:name/:key", function (req, res) {
     var name = req.params("name"),
       key = req.params("key");
-    res.json(repositories.structs.deleteDoc(name, key));
+    res.json(structs.deleteDoc(name, key));
   })
-  .nickname("content")
   .summary("Delete one entry")
   .notes("Delete one item of a collection.");
-  
+
   app.post("/content/:name/", function (req, res) {
     var name = req.params("name"),
       data = JSON.parse(req.requestBody);
-    res.json(repositories.structs.createDoc(name, data));
+    res.json(structs.createDoc(name, data));
   })
-  .nickname("content")
   .summary("Create a new entry")
   .notes("Create a new item in a collection.");
-  
-  
+
+
   app.get('/test/list', function (req, res) {
     res.json([{
       name: "String"
@@ -75,11 +69,10 @@
       name: "Number"
     }])
   })
-  .nickname("test")
   .summary("Test List")
   .notes("Test for List");
-  
-  
+
+
   app.get('/test/structs/:name', function (req, res) {
     var name = req.params("name");
     switch(name) {
@@ -123,10 +116,9 @@
         res.json("Error has to be done!");
     }
   })
-  .nickname("test")
   .summary("Test Structs")
   .notes("Test for Structs");
-  
+
   app.get('/test/content/:name', function (req, res) {
     var name = req.params("name");
     switch(name) {
@@ -168,9 +160,7 @@
         res.json("Error has to be done!");
     }
   })
-  .nickname("test")
   .summary("Test Content")
   .notes("Test for Content");
-  
-  app.start(applicationContext);
+
 }());
